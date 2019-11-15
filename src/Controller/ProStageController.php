bdaa1267427ad1f3ee31331e929b730d36c2d888
Leo Guirauton <lguirauton@iutbayonne.univ-pau.fr>
2019-11-15 09:18:54 +0100

    changement de la page d'accueil

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProStageController extends AbstractController
 {
     /**
-     * @Route("/pro/stage", name="pro_stage")
+     * @Route("/", name="prostage_accueil")
      */
     public function index()
     {
