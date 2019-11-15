197bea550358565ce0f9509cd8507e43153307a2
Leo Guirauton <lguirauton@iutbayonne.univ-pau.fr>
2019-11-15 08:57:57 +0100

    mise en place du controller metier

---
+++ src/Controller/ProStageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProStageController extends AbstractController
+{
+    /**
+     * @Route("/pro/stage", name="pro_stage")
+     */
+    public function index()
+    {
+        return $this->render('pro_stage/index.html.twig', [
+            'controller_name' => 'ProStageController',
+        ]);
+    }
+}
