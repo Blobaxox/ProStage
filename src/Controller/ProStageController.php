6b82286a2c8accb1d715e69b937e194db94c0cbf
Leo Guirauton <lguirauton@iutbayonne.univ-pau.fr>
2019-11-18 13:06:05 +0100

    ajouts des autre pages via Response

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProStageController extends AbstractController
 {
@@ -13,4 +14,20 @@ class ProStageController extends AbstractController
             'controller_name' => 'ProStageController',
         ]);
     }
+
+    public function entreprises()
+    {
+        return new Response("<html><body><h1>Cette page affichera la liste des entreprises proposant un stage</h1></body></html>");
+    }
+
+    public function formations()
+    {
+        return new Response("<html><body><h1>Cette page affichera la liste des formations de l'IUT</h1></body></html>");
+    }
+
+    public function stage($id)
+    {
+        return new Response("<html><body><h1>Cette page affichera le descriptif du stage ayant pour identifiant $id </h1></body></html>");
+    }
+
 }
